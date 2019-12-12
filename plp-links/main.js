bcf18c899f2c2f021dc491a5d9616ae367b28ea5
andreas <andreas.mansson1@ingka.ikea.com>
2019-12-12 14:32:35 +0100

    refactor and fixing eslint

--- plp-links/main.js
+++ plp-links/main.js
@@ -1,26 +1,27 @@
-iPlugins(function(lib){
+iPlugins(function (lib) {
 
-    
     function writePLPLink(item) {
-        if(item.icon != "undefined") {
-            var textlinkicon= lib.el('img', { style: "width:20px; margin-right:10px;", src: item.icon});
-            var textlinka= lib.el('a', { href: item.link}, [textlinkicon, item.text]);
-        }  else {
-            var textlinka= lib.el('a', { href: item.link}, [item.text]);
-        }  
-        
-        var textlinkdiv = lib.el('div', { style: "display:block;margin:0 auto;margin-top:20px;text-align:center;margin-bottom:20px;"}, [textlinka]);
-        
+        var textlinka;
+
+        if (item.icon !== 'undefined') {
+            var textlinkicon = lib.el('img', { style: 'width:20px; margin-right:10px;', src: item.icon });
+            textlinka = lib.el('a', { href: item.link }, [textlinkicon, item.text]);
+        } else {
+            textlinka = lib.el('a', { href: item.link }, [item.text]);
+        }
+
+        var textlinkdiv = lib.el('div', { style: 'display:block;margin:0 auto;margin-top:20px;text-align:center;margin-bottom:20px;' }, [textlinka]);
+
         lib.prepend(document.querySelector('.range-main-container'), textlinkdiv);
     }
 
     function workthedataPLPLink(data) {
-        if (document.readyState === "interactive" || document.readyState === "complete") {
+        if (document.readyState === 'interactive' || document.readyState === 'complete') {
             //if page is loaded, loop through data and make tags
-            data.Links.forEach(function(item, i){
-                if(window.location.href.indexOf(item.url) > -1) {
+            data.Links.forEach(function (item) {
+                if (window.location.href.indexOf(item.url) > -1) {
                     writePLPLink(item);
-                }    
+                }
             });
         } else {
             setTimeout(function () {
@@ -29,7 +30,7 @@ iPlugins(function(lib){
         }
     }
 
-    lib.getData('plp-links').then(function(data) { workthedataPLPLink(data); });
- 
-  
-  });
+    lib.getData('plp-links').then(function (data) {
+        workthedataPLPLink(data);
+    });
+});
