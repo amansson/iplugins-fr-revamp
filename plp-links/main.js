a5d729bc13efbacd713c96488c70bb08b658c7df
andreas <andreas.mansson1@ingka.ikea.com>
2019-12-12 15:57:41 +0100

    changed where links is added to the new rewamp page, now its add it under the title of the PLP. Also removed Icon from the implementation it still exist in the data source

--- plp-links/main.js
+++ plp-links/main.js
@@ -1,25 +1,16 @@
 iPlugins(function (lib) {
 
     function writePLPLink(item) {
-        var textlinka;
-
-        if (item.icon !== 'undefined') {
-            var textlinkicon = lib.el('img', { style: 'width:20px; margin-right:10px;', src: item.icon });
-            textlinka = lib.el('a', { href: item.link }, [textlinkicon, item.text]);
-        } else {
-            textlinka = lib.el('a', { href: item.link }, [item.text]);
-        }
-
-        var textlinkdiv = lib.el('div', { style: 'display:block;margin:0 auto;margin-top:20px;text-align:center;margin-bottom:20px;' }, [textlinka]);
-
-        lib.prepend(document.querySelector('.range-main-container'), textlinkdiv);
+        var textlinka = lib.el('a', { href: item.link }, [item.text]);
+        var textlinkdiv = lib.el('div', { style: 'text-align:left;margin-top:5px' }, [textlinka]);
+        lib.append(document.querySelector('.range-page-title'), textlinkdiv);
     }
 
     function workthedataPLPLink(data) {
         if (document.readyState === 'interactive' || document.readyState === 'complete') {
             //if page is loaded, loop through data and make tags
             data.Links.forEach(function (item) {
-                if (window.location.href.indexOf(item.url) > -1) {
+                if (window.location.href.indexOf(item.url) > -1 || window.location.href.replace('www2', 'www').indexOf(item.url) > -1) {
                     writePLPLink(item);
                 }
             });
