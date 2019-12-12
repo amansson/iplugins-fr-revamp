5cd1e52ca3b5af9ce8cb424997f9b52b1576b112
andreas <andreas.mansson1@ingka.ikea.com>
2019-12-12 14:30:09 +0100

    Initital iplugin start

---
+++ plp-links/main.js
@@ -0,0 +1,35 @@
+iPlugins(function(lib){
+
+    
+    function writePLPLink(item) {
+        if(item.icon != "undefined") {
+            var textlinkicon= lib.el('img', { style: "width:20px; margin-right:10px;", src: item.icon});
+            var textlinka= lib.el('a', { href: item.link}, [textlinkicon, item.text]);
+        }  else {
+            var textlinka= lib.el('a', { href: item.link}, [item.text]);
+        }  
+        
+        var textlinkdiv = lib.el('div', { style: "display:block;margin:0 auto;margin-top:20px;text-align:center;margin-bottom:20px;"}, [textlinka]);
+        
+        lib.prepend(document.querySelector('.range-main-container'), textlinkdiv);
+    }
+
+    function workthedataPLPLink(data) {
+        if (document.readyState === "interactive" || document.readyState === "complete") {
+            //if page is loaded, loop through data and make tags
+            data.Links.forEach(function(item, i){
+                if(window.location.href.indexOf(item.url) > -1) {
+                    writePLPLink(item);
+                }    
+            });
+        } else {
+            setTimeout(function () {
+                workthedataPLPLink(data);
+            }, 250);
+        }
+    }
+
+    lib.getData('plp-links').then(function(data) { workthedataPLPLink(data); });
+ 
+  
+  });
