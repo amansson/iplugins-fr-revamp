f146254a1c819dfaf53de8dd329b59654bf85a1a
andreas <andreas.mansson1@ingka.ikea.com>
2019-12-16 11:21:49 +0100

    refactor to minimize code and implement lib.ready with anonymous function call

--- plp-links/main.js
+++ plp-links/main.js
@@ -1,25 +1,23 @@
 iPlugins(function (lib) {
 
-    function writePLPLink(item) {
+    function writePLPLink(item, textElement) {
         var textlinka = lib.el('a', { href: item.link }, [item.text]);
         var textlinkdiv = lib.el('div', { style: 'text-align:left;margin-top:5px' }, [textlinka]);
-        lib.append(document.querySelector('.range-page-title'), textlinkdiv);
-    }
-
-    function workthedataPLPLink(data) {
-        if (document.readyState === 'interactive' || document.readyState === 'complete') {
-            //if page is loaded, loop through data and make tags
-            data.Links.forEach(function (item) {
-                if (window.location.href.indexOf(item.url) > -1) {
-                    writePLPLink(item);
-                }
-            });
-        } else {
-            lib.ready(workthedataPLPLink(data));
-        }
+        lib.append(textElement, textlinkdiv);
     }
 
     lib.getData('plp-links').then(function (data) {
-        workthedataPLPLink(data);
+        lib.ready(function () {
+            var textElement = document.querySelector('.range-page-title');
+            if (textElement) {
+                data.Links.forEach(function (item) {
+                    // Test on www2 for testing purpose
+                    // if (window.location.href.indexOf(item.url.replace(/https?:\/\/www.?\./,'')) > -1) {
+                    if (window.location.href.indexOf(item.url) > -1) {
+                        writePLPLink(item, textElement);
+                    }
+                });
+            }
+        });
     });
 });
