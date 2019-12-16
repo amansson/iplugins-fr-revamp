e8a94c030c93a8f8af187ea0bed0702fbffa21de
andreas <andreas.mansson1@ingka.ikea.com>
2019-12-16 09:23:55 +0100

    remove ww2 ref and timer, change to use iplugin: lib.ready

--- plp-links/main.js
+++ plp-links/main.js
@@ -10,14 +10,12 @@ iPlugins(function (lib) {
         if (document.readyState === 'interactive' || document.readyState === 'complete') {
             //if page is loaded, loop through data and make tags
             data.Links.forEach(function (item) {
-                if (window.location.href.indexOf(item.url) > -1 || window.location.href.replace('www2', 'www').indexOf(item.url) > -1) {
+                if (window.location.href.indexOf(item.url) > -1) {
                     writePLPLink(item);
                 }
             });
         } else {
-            setTimeout(function () {
-                workthedataPLPLink(data);
-            }, 250);
+            lib.ready(workthedataPLPLink(data));
         }
     }
 
